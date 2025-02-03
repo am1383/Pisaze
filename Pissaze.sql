e9a2759abf45bdfc059159568d344ab22b4d6838
am205379@gmail.com <am205379@gmail.com>
2025-02-03 09:19:24 +0330

    Append products tables

--- Pissaze.sql
+++ Pissaze.sql
@@ -8,3 +8,115 @@ CREATE TYPE transaction_enum AS ENUM ('successful', 'semi-successful', 'unsucces
 CREATE TYPE discount_enum AS ENUM ('public', 'private');
 CREATE TYPE cart_status_enum AS ENUM ('locked', 'registered', 'blocked');
 CREATE TYPE cooling_method_enum AS ENUM ('liquid', 'air');
+
+CREATE TABLE product (
+    id              INT PRIMARY KEY, 
+    brand           VARCHAR(50) NOT NULL,
+    model           VARCHAR(50) NOT NULL,
+    current_price   INT,
+    stock_count     SMALLINT,
+    category        VARCHAR(50),
+    product_image   BYTEA
+);
+
+CREATE TABLE hdd (
+    product_id          SERIAL PRIMARY KEY, 
+    capacity            DECIMAL(5, 2),          
+    rotational_speed    INT,  
+    wattage             INT,           
+    depth               DECIMAL(5, 2), 
+    height              DECIMAL(5, 2),    
+    width               DECIMAL(5, 2),
+    FOREIGN KEY (product_id) REFERENCES product (id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE gpu (
+    product_id  SERIAL PRIMARY KEY, 
+    ram_size    INT,         
+    clock_speed DECIMAL(5, 2), 
+    num_fans    SMALLINT,
+    wattage     INT,
+    depth       DECIMAL(5, 2), 
+    height      DECIMAL(5, 2),    
+    width       DECIMAL(5, 2),
+    FOREIGN KEY (product_id) REFERENCES product (id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE ram_stick (
+    product_id  SERIAL PRIMARY KEY, 
+    generation  VARCHAR(50),
+    capacity    DECIMAL(5, 2),    
+    frequency   DECIMAL(5, 2),   
+    wattage     INT, 
+    depth       DECIMAL(5, 2), 
+    height      DECIMAL(5, 2),    
+    width       DECIMAL(5, 2),   
+    FOREIGN KEY (product_id) REFERENCES product (id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE cooler (
+    product_id              SERIAL PRIMARY KEY, 
+    cooling_method          cooling_method_enum,
+    fan_size                INT,              
+    max_rotational_speed    INT,  
+    wattage                 INT,               
+    depth                   DECIMAL(5, 2), 
+    height                  DECIMAL(5, 2),    
+    width                   DECIMAL(5, 2),    
+    FOREIGN KEY (product_id) REFERENCES product (id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE motherboard (
+    product_id          SERIAL PRIMARY KEY, 
+    chipset_name        VARCHAR(50),
+    num_memory_slots    SMALLINT,
+    memory_speed_range  DECIMAL(5, 2),
+    wattage             INT,
+    depth               DECIMAL(5, 2), 
+    height              DECIMAL(5, 2),    
+    width               DECIMAL(5, 2),
+    FOREIGN KEY (product_id) REFERENCES product (id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE cpu (
+    product_id          SERIAL PRIMARY KEY, 
+    generation          VARCHAR(50),
+    microarchitecture   VARCHAR(50),
+    num_cores           SMALLINT,
+    num_threads         SMALLINT,
+    base_frequency      DECIMAL(5, 2), 
+    boost_frequency     DECIMAL(5, 2),
+    max_memory_limit    INT,         
+    wattage             INT,                
+    FOREIGN KEY (product_id) REFERENCES product (id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE case (
+    product_id      SERIAL PRIMARY KEY, 
+    product_type    VARCHAR(50),
+    color           VARCHAR(50),
+    material        VARCHAR(50),
+    fan_size        INT,         
+    num_fans        SMALLINT,
+    wattage         INT,
+    depth           DECIMAL(5, 2), 
+    height          DECIMAL(5, 2),    
+    width           DECIMAL(5, 2),
+    FOREIGN KEY (product_id) REFERENCES product (id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE power_supply (
+    product_id          SERIAL PRIMARY KEY, 
+    supported_wattage   INT,
+    depth               DECIMAL(5, 2), 
+    height              DECIMAL(5, 2),    
+    width               DECIMAL(5, 2),
+    FOREIGN KEY (product_id) REFERENCES product (id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE ssd (
+    product_id  SERIAL PRIMARY KEY, 
+    capacity    DECIMAL(5, 2), 
+    wattage     INT,
+    FOREIGN KEY (product_id) REFERENCES product (id) ON UPDATE CASCADE ON DELETE CASCADE
+);

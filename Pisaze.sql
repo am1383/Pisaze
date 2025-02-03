c7b7bb1d89681a25527d2eee6596dba87d68b49d
am205379@gmail.com <am205379@gmail.com>
2025-02-03 21:34:02 +0330

    Modify Cart_Enum

--- Pisaze.sql
+++ Pisaze.sql
@@ -8,7 +8,7 @@ CREATE DATABASE pisaze;
 
 CREATE TYPE transaction_enum AS ENUM ('successful', 'semi-successful', 'unsuccessful');
 CREATE TYPE discount_enum AS ENUM ('public', 'private');
-CREATE TYPE cart_enum AS ENUM ('locked', 'registered', 'blocked');
+CREATE TYPE cart_enum AS ENUM ('locked', 'blocked');
 CREATE TYPE cooling_enum AS ENUM ('liquid', 'air');
 
 --Create Tables
